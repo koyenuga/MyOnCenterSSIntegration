cc451a440e2093814d33a441ee7f428dc4c23cbc
Kamar Oyenuga <kamar.oyenuga@oncenter.com>
2018-02-22 18:25:32 -0600

    Zuora subscription command

---
+++ Oncenter.BackOffice.Zuora/Subscription/ZuoraCreateSubscriptionCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oncenter.BackOffice.Entities.Interfaces;
+using Oncenter.BackOffice.Zuora.Entities;
+
+namespace Oncenter.BackOffice.Zuora.Subscription
+{
+    public class ZuoraCreateSubscriptionCommand : ZuoraCommand, ICommand<ZuoraAccount, bool>
+    {
+        public bool Execute(ZuoraAccount request, IClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Rollback(ZuoraAccount request, IClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
